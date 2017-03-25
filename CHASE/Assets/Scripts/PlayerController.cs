d8865e10e4953d1c52c0de20f3090c6dc238a2a5
Pawel Borzym <pawel.borzym3095@gmail.com>
2017-03-25 23:08:47 +0000

    Player + Camera Scripts, Collision + Count, Basic UI

---
+++ CHASE/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private int count;
+    private int numberOfGameObjects;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        numberOfGameObjects = GameObject.FindGameObjectsWithTag("PickUp").Length;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= numberOfGameObjects)
+        {
+            winText.text = "YOU WIN!";
+        }
+    }
+}
