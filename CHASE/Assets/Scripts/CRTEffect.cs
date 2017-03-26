4c7a7a6e9711656c2a40101d0b80f39931c29b96
Pawel Borzym <pawel.borzym3095@gmail.com>
2017-03-26 22:35:36 +0100

    CRT Effect Script + Related CRT Shader to give that old screen look

---
+++ CHASE/Assets/Scripts/CRTEffect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+[ExecuteInEditMode]
+public class CRTEffect : PostEffectsBase
+{
+    public Shader shader = null;
+    private Material material;
+    public float Distortion = 0.1f;
+    public float InputGamma = 2.4f;
+    public float OutputGamma = 2.2f;
+    public float TextureSize = 768f;
+
+    public override bool CheckResources()
+    {
+        return shader.isSupported;
+    }
+
+    void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+    {
+        if (material == null)
+            material = new Material(shader);
+
+        if (material != null)
+        {
+            material.SetFloat("_Distortion", Distortion);
+            material.SetFloat("_InputGamma", InputGamma);
+            material.SetFloat("_OutputGamma", OutputGamma);
+            material.SetVector("_TextureSize", new Vector2(TextureSize, TextureSize));
+            Graphics.Blit(sourceTexture, destTexture, material);
+        }
+        else
+            Graphics.Blit(sourceTexture, destTexture);
+    }
+}
