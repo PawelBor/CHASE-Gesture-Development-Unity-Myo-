f461c7fe8575327c087552dab9b2c041cb80dfee
Pawel Borzym <pawel.borzym3095@gmail.com>
2017-03-27 07:34:15 +0100

    Moving Platform + its script + that 80's retro WireFrame Shader

---
+++ CHASE/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    [SerializeField]
+    Transform platform;
+
+    [SerializeField]
+    Transform startTransform;
+
+    [SerializeField]
+    Transform endTransform;
+
+    [SerializeField]
+    float platformSpeed;
+    //3d vectors + points, initialize direction
+    Vector3 direction;
+    Transform destination;
+
+    void Start()
+    {
+        SetDestination(startTransform);
+    }
+
+    //reccomended by Unityto do physics in fixedUpdate - ran in fixed time
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(platform.position + direction * platformSpeed * Time.fixedDeltaTime);
+       
+        if(Vector3.Distance (platform.position, destination.position) < platformSpeed * Time.fixedDeltaTime)
+        {
+            SetDestination(destination == startTransform ? endTransform : startTransform);
+        }
+    }
+
+    //Using gizmo just to help me visualize what position the platform will end up at during the development
+    //(inside editor view).
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(startTransform.position, platform.localScale);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(endTransform.position, platform.localScale);
+    }
+
+    void SetDestination(Transform dest)
+    {
+        destination = dest;
+        direction = (destination.position - platform.position).normalized;
+    }
+}
