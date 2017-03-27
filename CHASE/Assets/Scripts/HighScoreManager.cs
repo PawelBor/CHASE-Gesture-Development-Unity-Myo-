341cfa8713f5b14d7f6e7cf4408fe6d08365deae
ed <edvalas.la@gmail.com>
2017-03-27 14:30:18 +0100

    SQLite Database Scripts

---
+++ CHASE/Assets/Scripts/HighScoreManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class HighScoreManager : MonoBehaviour {
+
+    private string connString;
+
+    public List<HighScores> highScoresList = new List<HighScores>();
+
+	// Use this for initialization
+	void Start () {
+        connString = "URI=file:" + Application.dataPath + "/HighScoresDb.sqlite";
+        //InsertScores("Kek", 25, 12.3);
+        GetScores();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+    public void InsertScores(string level, int points, double time){
+
+        using (IDbConnection conn = new SqliteConnection(connString))
+        {
+            conn.Open();
+
+            using (IDbCommand cmd = conn.CreateCommand())
+            {
+                string sqlQuery = string.Format("INSERT INTO Scores(Level,Points,Time) VALUES(\"{0}\",\"{1}\",\"{2}\")",level,points,time);
+
+                cmd.CommandText = sqlQuery;
+                cmd.ExecuteScalar();
+                conn.Close();
+                
+            }
+
+        }
+    }
+
+    public void DeleteScores()
+    {
+        using (IDbConnection conn = new SqliteConnection(connString))
+        {
+            conn.Open();
+
+            using (IDbCommand cmd = conn.CreateCommand())
+            {
+                string sqlQuery = string.Format("DELETE FROM Scores");
+
+                cmd.CommandText = sqlQuery;
+                cmd.ExecuteScalar();
+                conn.Close();
+
+            }
+        }
+    }
+
+    private void GetScores()
+    {
+        highScoresList.Clear();
+
+        using (IDbConnection conn = new SqliteConnection(connString))
+        {
+            conn.Open();
+
+            using (IDbCommand cmd = conn.CreateCommand())
+            {
+                string sqlQuery = "SELECT * FROM Scores ORDER BY Time ASC LIMIT 10";
+
+                cmd.CommandText = sqlQuery;
+
+                using (IDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        highScoresList.Add(new HighScores(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetDouble(3)));
+                        //Debug.Log(reader.GetString(1) + reader.GetInt64(2) + reader.GetDouble(3));
+                        //https://www.youtube.com/watch?v=wV-dKxJU-0Y
+                    }
+                    conn.Close();
+                    reader.Close();
+                }
+            }
+
+        }
+    }
+}
---
+++ CHASE/Assets/Scripts/HighScores.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScores
+{
+    public int ID { get; set; }
+    public string Level { get; set; }
+    public int Points { get; set; }
+    public double Time { get; set; }
+
+    public HighScores(int id, string level, int points, double time) {
+        this.ID = id;
+        this.Level = level;
+        this.Points = points;
+        this.Time = time;
+    }
+}
